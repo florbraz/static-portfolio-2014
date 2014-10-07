94e82ba00ca1dddf4fdaeaec71f9343f4dcd4113
Flora Braz (casa) <eu@florbraz.com>
2014-10-07 01:36:18 -0300

    Angular + Drupal (RestWS): Reading URL of field_image

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,10 +11,20 @@ portfolioApp.controller('WorksCtrlr', function($scope, $http) {
 	else {
 		var urlJsonWorks = "js/works.json";
 	}
+
+  var gotImagesWorks = false;
   //$http.get('').
   $http.get(urlJsonWorks).
     success(function(data, status, headers, config) {
       $scope.works = data.list;
+      $.each($scope.works, function(index) {
+        if ($scope.works[index].field_image) {
+          var url = $scope.works[index].field_image.file.uri;
+          $http.get(url).success(function(data, status, headers, config) {
+            $scope.works[index].field_image.url = data.url;
+          });  
+        }
+      });
     }).
     error(function(data, status, headers, config) {
       // log error
